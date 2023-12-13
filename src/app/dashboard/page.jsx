89a76e0881a29f547a5b25ccfffd27d7dcf8007e
Deshun99 <e0560014@u.nus.edu>
2023-12-13 21:35:12 +0800

    mass push login register works one man army

---
+++ src/app/dashboard/page.jsx
@@ -0,0 +1,47 @@
+"use client";
+import React from "react";
+import styles from "./page.module.css";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { ProgressSpinner } from "primereact/progressspinner";
+
+const Dashboard = () => {
+  const session = useSession();
+
+  const router = useRouter();
+
+  const accessToken =
+    session.status === "authenticated" &&
+    session.data &&
+    session.data.user.accessToken;
+
+  const userId =
+    session.status === "authenticated" &&
+    session.data &&
+    session.data.user.userId;
+
+  const roleRef =
+    session.status === "authenticated" &&
+    session.data &&
+    session.data.user.role;
+
+  if (session.status === "loading") {
+    return <ProgressSpinner />;
+  }
+
+  if (session.status === "unauthenticated") {
+    router?.push("/login");
+  }
+
+  if (session.status === "authenticated") {
+    return (
+      <>
+        <h2 className={styles.header}>
+          Welcome Back {session.data.user.name}!
+        </h2>
+      </>
+    );
+  }
+};
+
+export default Dashboard;
