492b86a0d7abf3f0ff4f48885e2d7dbb2e471624
Deshun99 <e0560014@u.nus.edu>
2023-12-18 22:35:09 +0800

    status change for user

--- src/app/userManagement/page.jsx
+++ src/app/userManagement/page.jsx
@@ -6,16 +6,19 @@ import { useRouter } from "next/navigation";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 import { ProgressSpinner } from "primereact/progressspinner";
-import { getAllUsers } from "../api/user/route";
+import { getAllUsers, updateUser } from "../api/user/route";
 import { FilterMatchMode, FilterOperator } from "primereact/api";
 import { InputText } from "primereact/inputtext";
 import { Button } from "primereact/button";
 import { Dialog } from "primereact/dialog";
+import { Toast } from "primereact/toast";
+import Enums from "../common/enums/enums";
+import { Tooltip } from "primereact/tooltip";
 
 const UserManagement = () => {
   const session = useSession();
   const router = useRouter();
-
+  const toast = useRef(null);
   const accessToken =
     session.status === "authenticated" &&
     session.data &&
@@ -50,6 +53,7 @@ const UserManagement = () => {
   });
   const [globalFilterValue, setGlobalFilterValue] = useState("");
   const [viewUserDialog, setViewUserDialog] = useState(false);
+  const [userStatusDialog, setUserStatusDialog] = useState(false);
 
   const onGlobalFilterChange = (e) => {
     const value = e.target.value;
@@ -129,6 +133,8 @@ const UserManagement = () => {
               setSelectedRowData(rowData);
               showViewUserDialog(rowData);
             }}
+            tooltip="View Details"
+            tooltipOptions={{ position: "top" }}
           />
         </>
       );
@@ -142,8 +148,10 @@ const UserManagement = () => {
             className={styles.buttonIcon}
             onClick={() => {
               setSelectedRowData(rowData);
-              showUserDialog(rowData);
+              showUserStatusDialog(rowData);
             }}
+            tooltip="Update Status"
+            tooltipOptions={{ position: "top" }}
           />
           <Button
             icon="pi pi-search"
@@ -154,6 +162,8 @@ const UserManagement = () => {
               setSelectedRowData(rowData);
               setViewUserDialog(rowData);
             }}
+            tooltip="View Details"
+            tooltipOptions={{ position: "top" }}
           />
         </>
       );
@@ -167,6 +177,63 @@ const UserManagement = () => {
   const showViewUserDialog = (rowData) => {
     setViewUserDialog(true);
   };
+
+  const hideStatusDialog = () => {
+    setUserStatusDialog(false);
+  };
+
+  const showUserStatusDialog = (rowData) => {
+    setUserStatusDialog(true);
+  }
+
+  const saveStatusChange = async () => {
+    try {
+      const toggledStatus =
+        selectedRowData.status === Enums.ACTIVE ? Enums.INACTIVE : Enums.ACTIVE;
+      const request = {
+        status: toggledStatus,
+      };
+      const response = await updateUser(
+        request,
+        selectedRowData.userId,
+        accessToken
+      );
+
+      if (response) {
+        setSelectedRowData();
+        setUserStatusDialog(false);
+        toast.current.show({
+          severity: "success",
+          summary: "Success",
+          detail: `User ${selectedRowData.userId} updated successfully!`,
+          life: 5000,
+        });
+      }
+      setRefreshData((prev) => !prev);
+    } catch (error) {
+      console.log("Failed to update user");
+      toast.current.show({
+        severity: "error",
+        summary: "Error",
+        detail: error.message,
+        life: 5000,
+      });
+    }
+    // setSelectedRowData();
+    // setUserDialog(false);
+  };
+
+  const userDialogFooter = (
+    <React.Fragment>
+      <Button
+        label="Cancel"
+        icon="pi pi-times"
+        outlined
+        onClick={hideStatusDialog}
+      />
+      <Button label="Yes" icon="pi pi-check" onClick={saveStatusChange} />
+    </React.Fragment>
+  );
 
   if (session.status === "loading") {
     return <ProgressSpinner />;
@@ -183,13 +250,14 @@ const UserManagement = () => {
   if (session.status === "authenticated" && roleRef === "Admin") {
     return (
       <>
+        <Toast ref={toast} />
         <div
           className="datatable"
           style={{
             position: "relative",
             minHeight: "100%",
             width: "100%",
-            height: "600px"
+            height: "600px",
           }}
         >
           <DataTable
@@ -199,7 +267,7 @@ const UserManagement = () => {
             header={header}
             tableStyle={{
               width: "100%",
-              minHeight: "100px"
+              minHeight: "100px",
             }}
             rows={5}
             paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
@@ -222,46 +290,40 @@ const UserManagement = () => {
             emptyMessage="No users found."
             currentPageReportTemplate="Showing {first} to {last} of {totalRecords} entries"
           >
-            <Column
-              field="userName"
-              header="User Name"
-              sortable
-            ></Column>
-            <Column
-              field="email"
-              header="Email"
-              sortable
-            ></Column>
-            <Column
-              field="contactNo"
-              header="Contact No"
-              sortable
-            ></Column>
-            <Column
-              field="role"
-              header="Role"
-              sortable
-            ></Column>
-            <Column
-              field="status"
-              header="Active"
-              sortable
-            ></Column>
-            <Column
-              header="Action"
-              body={actionBody}
-            ></Column>
+            <Column field="userName" header="User Name" sortable></Column>
+            <Column field="email" header="Email" sortable></Column>
+            <Column field="contactNo" header="Contact No" sortable></Column>
+            <Column field="role" header="Role" sortable></Column>
+            <Column field="status" header="Active" sortable></Column>
+            <Column header="Action" body={actionBody}></Column>
           </DataTable>
 
           <Dialog
-                visible={viewUserDialog}
-                style={{ width: '32rem' }}
-                breakpoints={{ '960px': '75vw', '641px': '90vw' }}
-                header="User Details"
-                modal
-                className="p-fluid"
-                onHide={hideViewDialog}
+            visible={viewUserDialog}
+            style={{ width: "32rem" }}
+            breakpoints={{ "960px": "75vw", "641px": "90vw" }}
+            header="User Details"
+            modal
+            className="p-fluid"
+            onHide={hideViewDialog}
           ></Dialog>
+
+          <Dialog
+            visible={userStatusDialog}
+            style={{ width: "32rem" }}
+            breakpoints={{ "960px": "75vw", "641px": "90vw" }}
+            header={`Change Status to ${
+              selectedRowData &&
+              (selectedRowData.status === "Active" ? "Inactive" : "Active")
+            }`}
+            className="p-fluid"
+            footer={userDialogFooter}
+            onHide={hideStatusDialog}
+          >
+            <h3 className={styles.statusDialog}>
+              Are you sure you want to change the status of {selectedRowData && selectedRowData.userName}?
+            </h3>
+          </Dialog>
         </div>
       </>
     );
