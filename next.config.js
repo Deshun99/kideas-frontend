5a36a90204d4d66a2c436fa4ad78c1f44fc7b842
Deshun99 <e0560014@u.nus.edu>
2023-12-25 21:48:39 +0800

    next config changes

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: [],
-    },
-}
+  images: {
+    domains: ["https://kideas-upload.s3.ap-southeast-2.amazonaws.com"],
+  },
+};
 
 module.exports = nextConfig
